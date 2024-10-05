518e02ebb15c43905ba9ae9acbf5948221d27546
ivozandhuis <ivo@zandhuis.nl>
2024-10-05 17:32:24 +0200

    Added birth and death data

--- gedcom2pico.py
+++ gedcom2pico.py
@@ -11,7 +11,32 @@ from gedcom.element.source import SourceElement
 from gedcom.element.family import FamilyElement
 from gedcom.parser import Parser
 
-from rdflib import Graph, URIRef, Literal, BNode, Namespace, RDF, PROV, SDO
+from rdflib import Graph, URIRef, Literal, BNode, Namespace, RDF, PROV, SDO, XSD
+
+
+def date_converter(gedcom_date: str):
+    month_dict = {"JAN":"01","FEB":"02","MAR":"03","APR":"04","MAY":"05","JUN":"06",
+                    "JUL":"07","AUG":"08","SEP":"09","OCT":"10","NOV":"11","DEC":"12"}
+    date = gedcom_date.split(' ')
+    date.reverse()
+    ISOdate = ""
+    if len(date) == 3:
+        year = date[0]
+        month = date[1]
+        day = date[2]
+        if month in month_dict.keys():
+            ISOmonth = month_dict[month]
+        if len(day) == 1:
+            ISOday = "0" + day
+        else:
+            ISOday = day 
+        ISOdate = year + "-" + ISOmonth + "-" + ISOday
+    elif len(date) == 1:
+        year = date[0]
+        ISOdate = year
+
+    return ISOdate
+
 
 # Initialize file handling
 name = 'proef'
@@ -22,7 +47,7 @@ except: pass
 
 # Initialize rdflib variables
 baseUri = 'https://gen.example.com/' + name + '/'
-PICO = Namespace('https://personsincontext.org/model/')
+PICO = Namespace('https://personsincontext.org/model#')
 BIO = Namespace('http://purl.org/vocab/bio/0.1/')
 
 # Initialize the parser
@@ -65,35 +90,71 @@ for element in root_child_elements:
         # rdf:type
         g.add((subject, RDF.type, PICO.PersonReconstruction))
 
-        # schema:name
+        # sdo:name
         (first, last) = element.get_name()
         g.add((subject, SDO.name, Literal(first + " " + last)))
+
+        # sdo:birthDate
+        birth_date = element.get_birth_date()
+        if birth_date != "":
+            ISOdate = date_converter(birth_date)
+            if len(ISOdate) == 10:
+                datatype = XSD.date
+                g.add((subject, SDO.birthDate, Literal(ISOdate, datatype = datatype)))
+            elif len(ISOdate) == 4:
+                datatype = XSD.gYear
+                g.add((subject, SDO.birthDate, Literal(ISOdate, datatype = datatype)))
+
+        # sdo:birthPlace
+        birth_place = element.get_birth_place()
+        if birth_place != "":
+            g.add((subject, SDO.birthPlace, Literal(birth_place)))
+
+        # sdo:deathDate
+        death_date = element.get_death_date()
+        if death_date != "":
+            ISOdate = date_converter(death_date)
+            if len(ISOdate) == 10:
+                datatype = XSD.date
+            elif len(ISOdate) == 4:
+                datatype = XSD.gYear
+            g.add((subject, SDO.deathDate, Literal(ISOdate, datatype = datatype)))
+
+        # sdo:deathPlace
+        death_place = element.get_death_place()
+        if death_place != "":
+            g.add((subject, SDO.deathPlace, Literal(death_place)))
 
         # prov:wasDerivedFrom
         list = element.get_sources_by_tag_and_values(tag = gedcom.tags.GEDCOM_TAG_BIRTH)
         list = list + element.get_sources_by_tag_and_values(tag = gedcom.tags.GEDCOM_TAG_DEATH)
+        list = list + element.get_sources_by_tag_and_values(tag = gedcom.tags.GEDCOM_TAG_BURIAL)
+
+        child_elements = element.get_child_elements()
+        for child_element in child_elements:
+            tag = child_element.get_tag()
+            if tag == 'SOUR': list.append(child_element)
 
         for item in list:
             # Relate Blanknode for PICO.PersonObservation to subject
             blank_node = BNode()
             g.add((subject, PROV.wasDerivedFrom, blank_node))
-            
             # Add triples to the blank node
             g.add((blank_node, RDF.type, PICO.PersonObservation))
             src = item.get_value()[1:-1]
             g.add((blank_node, PROV.hadPrimarySource, URIRef(baseUri + src)))
 
-        # schema:spouse
+        # sdo:spouse
         for spouse in gedcom_parser.get_spouses(element):
             pointer = spouse.get_pointer()[1:-1]
             g.add((subject, SDO.spouse, URIRef(baseUri + pointer)))
 
-        #schema:children
+        # sdo:children
         for child in gedcom_parser.get_children(element):
             pointer = child.get_pointer()[1:-1]
             g.add((subject, SDO.children, URIRef(baseUri + pointer)))
 
-        # schema:gender
+        # sdo:gender
         gender = element.get_gender()
         if gender == 'F':
             g.add((subject, SDO.gender, SDO.Female))
@@ -106,11 +167,11 @@ for element in root_child_elements:
         # rdf:type
         g.add((subject, RDF.type, SDO.ArchiveComponent))
 
-        # schema:name
+        # sdo:name
         title = element.get_title()
         g.add((subject, SDO.name, Literal(title)))
 
-        # schema:url
+        # sdo:url
         child_elements = element.get_child_elements()
         for child_element in child_elements:
             tag = child_element.get_tag()
